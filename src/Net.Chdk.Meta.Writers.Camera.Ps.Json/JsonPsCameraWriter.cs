ca03bef9b459937e228e3128d9e088644d9d5296
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 10:07:26 +0300

    Net.Chdk.Meta.Writers.Camera.Json -> Net.Chdk.Meta.Writers.Json

--- src/Net.Chdk.Meta.Writers.Camera.Ps.Json/JsonPsCameraWriter.cs
+++ src/Net.Chdk.Meta.Writers.Camera.Ps.Json/JsonPsCameraWriter.cs
@@ -1,10 +1,10 @@
 ﻿using Net.Chdk.Meta.Model.Camera.Ps;
-using Net.Chdk.Meta.Writers.Camera.Json;
+using Net.Chdk.Meta.Writers.Json;
 using System.Collections.Generic;
 
 namespace Net.Chdk.Meta.Writers.Camera.Ps.Json
 {
-    sealed class JsonPsCameraWriter : JsonCameraWriter, IPsCameraWriter
+    sealed class JsonPsCameraWriter : JsonMetaWriter, IPsCameraWriter
     {
         public void WriteCameras(string path, IDictionary<string, PsCameraData> cameras)
         {
