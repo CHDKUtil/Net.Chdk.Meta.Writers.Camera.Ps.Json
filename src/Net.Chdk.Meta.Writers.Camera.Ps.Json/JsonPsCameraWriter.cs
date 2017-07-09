28636c6c3a6d85db3743747ecdabc606261b37a6
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 08:57:11 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Writers.Camera.Ps.Json/JsonPsCameraWriter.cs
@@ -0,0 +1,14 @@
+﻿using Net.Chdk.Meta.Model.Camera.Ps;
+using Net.Chdk.Meta.Writers.Camera.Json;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Writers.Camera.Ps.Json
+{
+    public sealed class JsonPsCameraWriter : JsonCameraWriter, IPsCameraWriter
+    {
+        public void WriteCameras(string path, IDictionary<string, PsCameraData> cameras)
+        {
+            WriteJson(path, cameras);
+        }
+    }
+}
